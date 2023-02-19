f74af01eca844aebe3668cef125c7ab29f7dfc03
dbasumatary <basudhondip@gmail.com>
2023-02-19 13:53:49 +0530

    Added a program to find out the maximum among string values

--- src/com/genericsproblem/MaximumTest.java
+++ src/com/genericsproblem/MaximumTest.java
@@ -1,6 +1,6 @@
 package com.genericsproblem;
 import java.lang.*;
-/*Maximum of three float numbers using generics*/
+/*Maximum of three values using generics*/
 
 public class MaximumTest <T extends Comparable<T>>{        //It ensures the object of type T are comparable using interface
     public static <T extends Comparable<T>> void maximumValue(T input1, T input2, T input3){
@@ -17,5 +17,6 @@ public class MaximumTest <T extends Comparable<T>>{        //It ensures the obje
     public static void main(String[] args) {
         maximumValue(34,33,35);                              //calling the method for integers
         maximumValue(6.21,6.19,6.24);                        //calling the method for float values
+        maximumValue("Apple","Peach","Banana");              //calling the method for string values
     }
 }
