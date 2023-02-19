b9b68ac2c828340f64075cfc33e49e595592deb9
dbasumatary <basudhondip@gmail.com>
2023-02-19 15:00:26 +0530

    Added a program to find out the maximum among multiple values

--- src/com/genericsproblem/MaximumTest.java
+++ src/com/genericsproblem/MaximumTest.java
@@ -1,22 +1,22 @@
 package com.genericsproblem;
 import java.lang.*;
-/*Maximum of three values using generics*/
+/*Maximum of multiple values using generics*/
 
 public class MaximumTest <T extends Comparable<T>>{        //It ensures the object of type T are comparable using interface
-    public static <T extends Comparable<T>> void maximumValue(T input1, T input2, T input3){
+    @SafeVarargs                           //this annotation is used to suppress unsafe operation warnings at compile time
+    public static <T extends Comparable<T>> void maximumValue(T ... elements){            //multiple values as arguments
 
-        T maximum = input1;                               //Assuming input1 is the largest initially
-        if (input2.compareTo(maximum) > 0){
-            maximum = input2;                             //Now input2 is the largest
+        T maximum = elements[0];                               //Assuming element in index 0 is the largest initially
+        for (T element : elements){                            //using for loop for every element
+            if (element.compareTo(maximum) > 0){
+                maximum = element;
+            }
         }
-        if (input3.compareTo(maximum) > 0) {
-            maximum = input3;                             //input3 is the largest
-        }
-        System.out.println("Maximum of "+ input1 +", "+ input2 +" and "+ input3 +" is " + maximum);
+        System.out.println("The maximum value among these elements is " + maximum);
     }
     public static void main(String[] args) {
-        maximumValue(34,33,35);                              //calling the method for integers
-        maximumValue(6.21,6.19,6.24);                        //calling the method for float values
-        maximumValue("Apple","Peach","Banana");              //calling the method for string values
+        maximumValue(56, 21, 8, 73, 57, 78, 40);                          //calling the method for integers
+        maximumValue(5.9, 34.6, 24.4, 41.6, 19.25, 41.6001);                 //calling the method for float values
+        maximumValue( "Mango", "Apple", "Guava", "Orange", "Date");        //calling the method for string values
     }
 }
